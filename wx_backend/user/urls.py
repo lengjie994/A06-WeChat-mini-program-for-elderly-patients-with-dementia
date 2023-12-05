17e65f3419125c79032a3b610c8fa12573b1df18
lengjie <120730091+wuhdhfd@users.noreply.github.com>
2023-12-05 22:09:44 +0800

    Add files via upload

--- wx_backend/user/urls.py
+++ wx_backend/user/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from user.views import WeixinLogin
 from user.views import ChooseRole
 from user.views import GetPatientInfo
+from user.views import ModifyMemorial
+from user.views import GuardianToPatient
+from user.views import SendHealthdata
+from user.views import SendMemoir
+from user.views import SendReminder
 
 app_name = 'user'   #后续接口的名称
 
@@ -9,4 +14,9 @@ urlpatterns = [
     path('login/', WeixinLogin.as_view(), name='login'),
     path('role/', ChooseRole.as_view(), name='role'),
     path('getPatientInfo/', GetPatientInfo.as_view(), name='getPatientInfo'),
+    path('modifyMemorial/', ModifyMemorial.as_view(), name='modifyMemorial'),
+    path('GuardianToPatient/', GuardianToPatient.as_view(), name='GuardianToPatient'),
+    path('SendHealthdata/', SendHealthdata.as_view(), name='SendHealthdata'),
+    path('SendMemoir/', SendMemoir.as_view(), name='SendMemoir'),
+    path('SendReminder/', SendReminder.as_view(), name='SendReminder'),
 ]
